7dc42e4029509b32939ccb3b9a57e8ab7a3c9f3e
insanefury <justdarsan@gmail.com>
2019-03-25 22:12:11 -0300

    added planet rotation & ring

---
+++ Assets/Scripts/planet_offset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planet_offset : MonoBehaviour
+{
+    public float speed;
+    public bool to_right_side;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (to_right_side) {
+            transform.Rotate(0, speed * Time.deltaTime, 0);
+        }
+        else {
+            transform.Rotate(0, -(speed * Time.deltaTime), 0);
+        }
+        
+    }
+}
